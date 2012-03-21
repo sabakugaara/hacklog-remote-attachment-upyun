18ffad7c3068192d29ac8b81f0bfedc9c300fedc
HuangYeWuDeng <huangyewudeng@vip.qq.com>
2012-03-21 08:33:55 +0800

    first commit

---
+++ includes/upyun.class.php
@@ -0,0 +1,642 @@
+<?php
+if(!defined('ABSPATH'))
+{
+	die('What are you doing?');
+}
+
+class UpYun {
+	const VERION = 'ihacklog_20120206';
+	private $bucketname;
+	private $username;
+	private $password;
+	private $api_domain = 'v0.api.upyun.com';
+	public $timeout = 30;
+	public $debug = false;
+	public $errors;
+	public static $http;
+	
+	/**
+	 * 初始化 UpYun 存储接口
+	 * $bucketname 空间名称
+	 * $username 操作员名称
+	 * $password 密码
+	 * 
+	 * @param $args unknown_type
+	 *       	 return UpYun object
+	 */
+	public function __construct($args) {
+		$new_args = array_merge ( array ('api_domain' => $this->api_domain, 'bucketname' => '', 'username' => '', 'password' => '', 'timeout' => '' ), $args );
+		
+		$this->bucketname = $new_args ['bucketname'];
+		$this->username = $new_args ['username'];
+		$this->password = md5 ( $new_args ['password'] );
+		$this->setTimeout ( $new_args ['timeout'] );
+		$this->setApiDomain ( $new_args ['api_domain'] );
+		$this->errors = new WP_Error();
+		$this->set_debug ( FALSE );
+		$this->check_param ();
+	}
+	
+	/**
+	 * check the needed param
+	 */
+	public function check_param() {
+		if (empty ( $this->api_domain )) {
+			$this->errors->add ( 'empty_api_domain', __ ( 'api_domain is required' ) );
+		}
+		
+		if (empty ( $this->bucketname )) {
+			$this->errors->add ( 'empty_bucketname', __ ( 'bucketname is required' ) );
+		}
+		
+		if (empty ( $this->username )) {
+			$this->errors->add ( 'empty_username', __ ( 'username is required' ) );
+		}
+		if (empty ( $this->password )) {
+			$this->errors->add ( 'empty_password', __ ( 'password is required' ) );
+		}
+	}
+	
+	/**
+	 * 切换 API 接口的域名
+	 * 
+	 * @param $domain {默认
+	 *       	 v0.api.upyun.com 自动识别, v1.api.upyun.com 电信, v2.api.upyun.com
+	 *        	联通, v3.api.upyun.com 移动}
+	 *       	 return null;
+	 */
+	public function setApiDomain($domain) {
+		$this->api_domain = $domain;
+	}
+	
+	/**
+	 * 设置连接超时时间
+	 * 
+	 * @param $time 秒
+	 *       	 return null;
+	 */
+	public function setTimeout($time) {
+		$this->timeout = $time;
+	}
+	
+	/**
+	 * 连接签名方法
+	 * 
+	 * @param $method 请求方式
+	 *       	 {GET, POST, PUT, DELETE}
+	 * @return 签名字符串
+	 */
+	private function sign($method, $uri, $date, $length) {
+		$sign = "{$method}&{$uri}&{$date}&{$length}&{$this->password}";
+		return 'UpYun ' . $this->username . ':' . md5 ( $sign );
+	}
+	
+	/**
+	 * 连接处理逻辑
+	 * @todo 添加output_file支持( 荒野无灯 2012-02-06 )
+	 * 
+	 * @param process_url 请求方式
+	 *       	 {GET, POST, PUT, DELETE}
+	 * @param process_url地址       	
+	 * @param process_url如果是
+	 *       	 POST 上传文件，传递文件内容 或 文件IO数据流
+	 * @param process_url_file 如果是
+	 *       	 GET 下载文件，可传递文件IO数据流
+	 * @return 请求返回字符串，失败返回 null （打开 debug 状态下遇到错误将中止程序执行）
+	 */
+	private function HttpAction($method, $uri, $datas, $output_file = null) 
+	{
+		$http = self::get_http_object();
+		$r = array();
+		if( is_wp_error($http))
+		{
+			$this->errors = $http;
+			return FALSE;
+		}
+		//check if the relative path string is started with a '/' ,since the upyun API need this
+		if ('/' != substr ( $uri, 0, 1 )) {
+			$uri = '/' . $uri;
+		}
+		
+		$uri = "/{$this->bucketname}{$uri}";
+		$process_url = "http://{$this->api_domain}{$uri}";
+// 		$process_url = curl_init("http://{$this->api_domain}{$uri}");
+		$headers = array ('Expect'=>'' );
+		if ($datas == 'folder:true') 
+		{
+			$headers['folder'] = 'true';
+			$datas = "\n";
+		}
+		$length = @strlen ( $datas );
+		if ($method == 'PUT' || $method == 'POST') 
+		{
+			if ($this->auto_mkdir == true) {
+				$headers ['mkdir'] = 'true';
+		}
+			$method = 'POST';
+// 			curl_setopt ( $process_url, CURLOPT_POST, 1 );
+			if ($datas && $datas != 'folder:true') {
+				if (is_resource ( $datas )) {
+					$handle = $datas;
+					fseek ( $handle, 0, SEEK_END );
+					$length = ftell ( $handle );
+					fseek ( $handle, 0 );
+					$headers ['Content-Length'] = $length;
+					
+					$datas = fread($handle,$length);
+					fclose($handle);
+					$r['body'] = $datas ;
+					
+				} else
+					$r['body'] = $datas;
+			}
+		}
+		
+		$date = gmdate ( 'D, d M Y H:i:s \G\M\T' );
+		$headers ['Date'] = $date;
+		$headers ['Authorization'] = $this->sign ( $method, $uri, $date, $length );
+		
+// 		var_dump($headers);
+		$r['headers'] = $headers;
+		$r['timeout'] = $this->timeout ;
+// 		if (is_resource ( $output_file ))
+// 			curl_setopt ( $process_url, CURLOPT_FILE, $output_file );
+		$http_method = strtolower($method);
+		$valid_method = array('head','get','post','put','delete');
+		if( !in_array($http_method, $valid_method ))
+		{
+			$this->errors->add ( 'invalid_call_of_http_method_error', __ ( 'invalid http method(valid method can be: '. implode(',',$valid_method) .')' ) );
+			return FALSE;
+		}
+		@set_time_limit(300);
+		$response = $http->$http_method( $process_url,$r );
+		$communication_error = 'Can not connect to remote server!';
+		if( is_wp_error($response))
+		{
+			$this->errors->add ( 'communication_error', __ ( $communication_error  ) );
+			return FALSE;
+		}
+// 		var_dump($response);
+		$response_code = $response['response']['code'];
+		
+		if ($response_code != 200) {
+			if ($this->debug) {
+				throw new Exception ( $response['response']['message'], $response_code );
+			} else {
+				//store the error.
+				if( empty($response['response']['message']) )
+				{
+					$response['response']['message'] = 'Can not connect to remote server!';
+				}
+				$this->errors->add ( 'communication_error', __ ( $response['response']['message'] ) );
+			}
+			return null;
+		}
+		return $response['body'];
+	}
+	
+	/**
+	 * 获取总体空间的占用信息
+	 * @return 空间占用量，失败返回 null
+	 */
+	public function getBucketUsage() {
+		return $this->getFolderUsage ( '/' );
+	}
+	
+	/**
+	 * 获取某个子目录的占用信息
+	 * 
+	 * @param $path 目标路径
+	 * @return 空间占用量，失败返回 null
+	 */
+	public function getFolderUsage($path) {
+		$r = $this->HttpAction ( 'GET', "{$path}?usage", null );
+		if ($r == '')
+			return null;
+		return floatval ( $r );
+	}
+	
+	/**
+	 * 上传文件
+	 * 
+	 * @param $file 文件路径（包含文件名）       	
+	 * @param $datas 文件内容
+	 *       	 或 文件IO数据流
+	 * @param $auto_mkdir=false 是否自动创建父级目录
+	 *       	 return true or false
+	 */
+	public function writeFile($file, $datas, $auto_mkdir = false) {
+		$this->auto_mkdir = $auto_mkdir;
+		$r = $this->HttpAction ( 'PUT', $file, $datas );
+		return ! is_null ( $r );
+	}
+	
+	/**
+	 * 读取文件
+	 * 
+	 * @param $file 文件路径（包含文件名）       	
+	 * @param $output_file 可传递文件IO数据流（默认为
+	 *       	 null，结果返回文件内容，如设置文件数据流，将返回 true or false）
+	 *       	 return 文件内容 或 null
+	 */
+	public function readFile($file, $output_file = null) {
+		return $this->HttpAction ( 'GET', $file, null, $output_file );
+	}
+	
+	/**
+	 * 读取目录列表
+	 * 
+	 * @param $path 目录路径
+	 * @return array 数组 或 null
+	 */
+	public function readDir($path) {
+		$r = $this->HttpAction ( 'GET', $path, null );
+		if (is_null ( $r ))
+			return null;
+		$rs = explode ( "\n", $r );
+		$returns = array ();
+		foreach ( $rs as $r ) {
+			$r = trim ( $r );
+			$l = new stdclass ();
+			list ( $l->name, $l->type, $l->size, $l->time ) = explode ( "\t", $r );
+			if (! empty ( $l->time )) {
+				$l->type = ($l->type == 'N' ? 'file' : 'folder');
+				$l->size = intval ( $l->size );
+				$l->time = intval ( $l->time );
+				$returns [] = $l;
+			}
+		}
+		return $returns;
+	}
+	
+	/**
+	 * 删除文件
+	 * 
+	 * @param $file 文件路径（包含文件名）
+	 * @return true or false
+	 */
+	public function deleteFile($file) {
+		$r = $this->HttpAction ( 'DELETE', $file, null );
+		return ! is_null ( $r );
+	}
+	
+	/**
+	 * 创建目录
+	 * 
+	 * @param $path 目录路径       	
+	 * @param $auto_mkdir=false 是否自动创建父级目录
+	 *       	 return true or false
+	 */
+	public function mkDir($path, $auto_mkdir = false) {
+		$this->auto_mkdir = $auto_mkdir;
+		$r = $this->HttpAction ( 'PUT', $path, 'folder:true' );
+		return ! is_null ( $r );
+	}
+	
+	/**
+	 * 删除目录
+	 * 
+	 * @param $path 目录路径
+	 * @return true or false
+	 */
+	public function rmDir($dir) {
+		$r = $this->HttpAction ( 'DELETE', $dir, null );
+		return ! is_null ( $r );
+	}
+	
+	////////////////////////compatible//////////////////////////
+	
+	/**
+	 * 
+	 * @param bool $debug 是否调试
+	 */
+	public function set_debug($debug) {
+		$this->debug = ( bool ) $debug;
+	}
+	
+	/**
+	 * check if we can connect to the server correctly
+	 * since Upyun does not have the API,here I just check if we can 
+	 * get the total bucket space usage.
+	 * @return bool
+	 */
+	public function check_rest_connection() {
+		if( gethostbyname($this->api_domain ) == $this->api_domain )
+		{
+			$this->errors->add ( 'connect', __('Failed to connect to remote server!') );
+			return FALSE;
+		}
+		if (null == $this->getBucketUsage()) {
+			$this->errors->add ( 'connect', __ ( 'Could not communicate with upyun server!' ) );
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+	
+	/**
+	 * fake function ,does nothing,since the upload API allows auto make dir.
+	 */
+	public function is_dir($dir) {
+		return TRUE;
+	}
+	
+	/**
+	 * check if a file is exists.
+	 * @param string $file the full file path relative to the bucket main dir. 
+	 */
+	public function is_file($file) {
+		$file_content = $this->readFile ( $file );
+		if (! empty ( $file_content )) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * get the remote file size
+	 * @param string $file the full file path relative to the bucket main dir.
+	 */
+	public function size($file) {
+		$file_content = $this->readFile ( $file );
+		return strlen ( $file_content );
+	}
+	
+	/**
+	 * the same as is_file.used by hacklogra_upyun::decrease_filesize_used()
+	 * @param unknown_type $file
+	 */
+	public function exists($file) {
+		return $this->is_file ( $file );
+	}
+	
+	/**
+	 * for uploading file to remote server.used by class hacklogra_upyun
+	 * @param string $file the full file path relative to the bucket main dir.
+	 * @param unknown_type $data
+	 * @param int $perm ,this param is not used by upyun server...
+	 */
+	public function put_contents($file, $data, $perm = 0744) {
+		return $this->writeFile ( $file, $data, TRUE );
+	}
+	
+	/**
+	 * for deleting file on remote server.used by class hacklogra_upyun
+	 * @param string $file the full file path relative to the bucket main dir.
+	 * @param unknown_type $notused this param is not used by upyun server...
+	 * @param unknown_type $another_notused this param is not used by upyun server...
+	 */
+	public function delete($file, $notused = '', $another_notused = '') {
+		return $this->deleteFile ( $file );
+	}
+	
+	static function &get_http_object() {
+		if ( is_null(self::$http) )
+		{
+			self::$http = new hacklog_http();
+		}
+		return self::$http;
+	}
+
+}//end class UpYun
+
+
+
+
+class hacklog_http extends WP_Http
+{
+	public function _get_first_available_transport( $args, $url = null ) {
+		$request_order = array( 'curl', 'streams', 'fsockopen' );
+	
+		// Loop over each transport on each HTTP request looking for one which will serve this request's needs
+		foreach ( $request_order as $transport ) {
+			$class = 'hacklog_http_' . $transport;
+	
+			// Check to see if this transport is a possibility, calls the transport statically
+			if ( !call_user_func( array( $class, 'test' ), $args, $url ) )
+				continue;
+	
+			return $class;
+		}
+	
+		return false;
+	}
+	
+	function put($url, $args = array()) {
+		$defaults = array('method' => 'PUT');
+		$r = wp_parse_args( $args, $defaults );
+		return $this->request($url, $r);
+	}
+	
+	function delete($url, $args = array())
+	{
+		$defaults = array('method' => 'DELETE');
+		$r = wp_parse_args( $args, $defaults );
+		return $this->request($url, $r);
+	}
+	
+}//end class hacklog_http
+
+class hacklog_http_fsockopen extends WP_Http_Fsockopen
+{
+	
+}
+
+class hacklog_http_streams extends WP_Http_Streams
+{
+	
+}
+
+class hacklog_http_curl extends WP_Http_Curl
+{
+	/**
+	 * private method can not be called from child class,so, I just copied it from the parent class.
+	 * Grab the headers of the cURL request
+	 *
+	 * Each header is sent individually to this callback, so we append to the $header property for temporary storage
+	 *
+	 * @since 3.2.0
+	 * @access private
+	 * @return int
+	 */
+	private function stream_headers( $handle, $headers ) {
+		$this->headers .= $headers;
+		return strlen( $headers );
+	}
+	
+	/**
+	 * Send a HTTP request to a URI using cURL extension.
+	 * added DELETE method.(荒野无灯 2012-02-06)
+	 *
+	 * @access public
+	 * @since 2.7.0
+	 *
+	 * @param string $url
+	 * @param str|array $args Optional. Override the defaults.
+	 * @return array 'headers', 'body', 'response', 'cookies' and 'filename' keys.
+	 */
+	function request($url, $args = array()) {
+		$defaults = array(
+				'method' => 'GET', 'timeout' => 5,
+				'redirection' => 5, 'httpversion' => '1.0',
+				'blocking' => true,
+				'headers' => array(), 'body' => null, 'cookies' => array()
+		);
+	
+		$r = wp_parse_args( $args, $defaults );
+	
+		if ( isset($r['headers']['User-Agent']) ) {
+			$r['user-agent'] = $r['headers']['User-Agent'];
+			unset($r['headers']['User-Agent']);
+		} else if ( isset($r['headers']['user-agent']) ) {
+			$r['user-agent'] = $r['headers']['user-agent'];
+			unset($r['headers']['user-agent']);
+		}
+	
+		// Construct Cookie: header if any cookies are set.
+		WP_Http::buildCookieHeader( $r );
+	
+		$handle = curl_init();
+	
+		// cURL offers really easy proxy support.
+		$proxy = new WP_HTTP_Proxy();
+	
+		if ( $proxy->is_enabled() && $proxy->send_through_proxy( $url ) ) {
+	
+			curl_setopt( $handle, CURLOPT_PROXYTYPE, CURLPROXY_HTTP );
+			curl_setopt( $handle, CURLOPT_PROXY, $proxy->host() );
+			curl_setopt( $handle, CURLOPT_PROXYPORT, $proxy->port() );
+	
+			if ( $proxy->use_authentication() ) {
+				curl_setopt( $handle, CURLOPT_PROXYAUTH, CURLAUTH_ANY );
+				curl_setopt( $handle, CURLOPT_PROXYUSERPWD, $proxy->authentication() );
+			}
+		}
+	
+		$is_local = isset($args['local']) && $args['local'];
+		$ssl_verify = isset($args['sslverify']) && $args['sslverify'];
+		if ( $is_local )
+			$ssl_verify = apply_filters('https_local_ssl_verify', $ssl_verify);
+		elseif ( ! $is_local )
+		$ssl_verify = apply_filters('https_ssl_verify', $ssl_verify);
+	
+	
+		// CURLOPT_TIMEOUT and CURLOPT_CONNECTTIMEOUT expect integers.  Have to use ceil since
+		// a value of 0 will allow an unlimited timeout.
+		$timeout = (int) ceil( $r['timeout'] );
+		curl_setopt( $handle, CURLOPT_CONNECTTIMEOUT, $timeout );
+		curl_setopt( $handle, CURLOPT_TIMEOUT, $timeout );
+	
+		curl_setopt( $handle, CURLOPT_URL, $url);
+		curl_setopt( $handle, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $handle, CURLOPT_SSL_VERIFYHOST, ( $ssl_verify === true ) ? 2 : false );
+		curl_setopt( $handle, CURLOPT_SSL_VERIFYPEER, $ssl_verify );
+		curl_setopt( $handle, CURLOPT_USERAGENT, $r['user-agent'] );
+		curl_setopt( $handle, CURLOPT_MAXREDIRS, $r['redirection'] );
+	
+		switch ( $r['method'] ) {
+			case 'HEAD':
+				curl_setopt( $handle, CURLOPT_NOBODY, true );
+				break;
+			case 'POST':
+				curl_setopt( $handle, CURLOPT_POST, true );
+				curl_setopt( $handle, CURLOPT_POSTFIELDS, $r['body'] );
+				break;
+			case 'PUT':
+				curl_setopt( $handle, CURLOPT_CUSTOMREQUEST, 'PUT' );
+				curl_setopt( $handle, CURLOPT_POSTFIELDS, $r['body'] );
+				break;
+			case 'DELETE':
+				curl_setopt( $handle, CURLOPT_CUSTOMREQUEST, 'DELETE' );
+				curl_setopt( $handle, CURLOPT_NOBODY, true );
+				break;					
+		}
+	
+		if ( true === $r['blocking'] )
+			curl_setopt( $handle, CURLOPT_HEADERFUNCTION, array( &$this, 'stream_headers' ) );
+	
+		curl_setopt( $handle, CURLOPT_HEADER, false );
+	
+		// If streaming to a file open a file handle, and setup our curl streaming handler
+		if ( $r['stream'] ) {
+			if ( ! WP_DEBUG )
+				$stream_handle = @fopen( $r['filename'], 'w+' );
+			else
+				$stream_handle = fopen( $r['filename'], 'w+' );
+			if ( ! $stream_handle )
+				return new WP_Error( 'http_request_failed', sprintf( __( 'Could not open handle for fopen() to %s' ), $r['filename'] ) );
+			curl_setopt( $handle, CURLOPT_FILE, $stream_handle );
+		}
+	
+		// The option doesn't work with safe mode or when open_basedir is set.
+		if ( !ini_get('safe_mode') && !ini_get('open_basedir') && 0 !== $r['_redirection'] )
+			curl_setopt( $handle, CURLOPT_FOLLOWLOCATION, true );
+	
+		if ( !empty( $r['headers'] ) ) {
+			// cURL expects full header strings in each element
+			$headers = array();
+			foreach ( $r['headers'] as $name => $value ) {
+				$headers[] = "{$name}: $value";
+			}
+			curl_setopt( $handle, CURLOPT_HTTPHEADER, $headers );
+		}
+	
+		if ( $r['httpversion'] == '1.0' )
+			curl_setopt( $handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0 );
+		else
+			curl_setopt( $handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1 );
+	
+		// Cookies are not handled by the HTTP API currently. Allow for plugin authors to handle it
+		// themselves... Although, it is somewhat pointless without some reference.
+		do_action_ref_array( 'http_api_curl', array(&$handle) );
+	
+		// We don't need to return the body, so don't. Just execute request and return.
+		if ( ! $r['blocking'] ) {
+			curl_exec( $handle );
+			curl_close( $handle );
+			return array( 'headers' => array(), 'body' => '', 'response' => array('code' => false, 'message' => false), 'cookies' => array() );
+		}
+	
+		$theResponse = curl_exec( $handle );
+		$theBody = '';
+		$theHeaders = WP_Http::processHeaders( $this->headers );
+	
+		if ( strlen($theResponse) > 0 && ! is_bool( $theResponse ) ) // is_bool: when using $args['stream'], curl_exec will return (bool)true
+			$theBody = $theResponse;
+	
+		// If no response, and It's not a HEAD request with valid headers returned
+		if ( 0 == strlen($theResponse) && ('HEAD' != $args['method'] || empty($this->headers)) ) {
+			if ( $curl_error = curl_error($handle) )
+				return new WP_Error('http_request_failed', $curl_error);
+			if ( in_array( curl_getinfo( $handle, CURLINFO_HTTP_CODE ), array(301, 302) ) )
+				return new WP_Error('http_request_failed', __('Too many redirects.'));
+		}
+	
+		$this->headers = '';
+	
+		$response = array();
+		$response['code'] = curl_getinfo( $handle, CURLINFO_HTTP_CODE );
+		$response['message'] = get_status_header_desc($response['code']);
+	
+		curl_close( $handle );
+	
+		if ( $r['stream'] )
+			fclose( $stream_handle );
+	
+		// See #11305 - When running under safe mode, redirection is disabled above. Handle it manually.
+		if ( ! empty( $theHeaders['headers']['location'] ) && ( ini_get( 'safe_mode' ) || ini_get( 'open_basedir' ) ) && 0 !== $r['_redirection'] ) {
+			if ( $r['redirection']-- > 0 ) {
+				return $this->request( $theHeaders['headers']['location'], $r );
+			} else {
+				return new WP_Error( 'http_request_failed', __( 'Too many redirects.' ) );
+			}
+		}
+	
+		if ( true === $r['decompress'] && true === WP_Http_Encoding::should_decode($theHeaders['headers']) )
+			$theBody = WP_Http_Encoding::decompress( $theBody );
+	
+		return array( 'headers' => $theHeaders['headers'], 'body' => $theBody, 'response' => $response, 'cookies' => $theHeaders['cookies'], 'filename' => $r['filename'] );
+	}
+	
+}//end class hacklog_http_curl
---
+++ loader.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: Hacklog Remote Attachment Upyun
+Plugin URI: http://ihacklog.com/?p=5204 
+Description: WordPress 远程附件上传插件for Upyun(又拍云).Remote attachment support for WordPress.Support Multisite. 
+Version: 1.2.9 
+Author: 荒野无灯 
+Author URI: http://ihacklog.com/
+*/
+
+/**
+ * $Id: loader.php 477024 2011-12-18 07:24:47Z ihacklog $
+ * $Revision: 477024 $
+ * $Date: 2011-12-18 07:24:47 +0000 (Sun, 18 Dec 2011) $
+ * 
+ * @package Hacklog Remote Attachment
+ *          @encoding UTF-8
+ * @author 荒野无灯 <HuangYeWuDeng>
+ * @link http://ihacklog.com
+ * @copyright Copyright (C) 2011 荒野无灯
+ * @license http://www.gnu.org/licenses/
+ */
+
+/*
+ Copyright 2011  荒野无灯
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+if (! defined ( 'ABSPATH' )) {
+	die ( 'What are you doing?' );
+}
+
+//the Hacklog Remote Attachment plugin maybe loaded after this plugin,so,we can only remind the user.
+function check_hacklog_ra_upyun_compability()
+{
+if (class_exists ( 'hacklogra' ))
+{
+	add_action ( 'admin_notices', create_function ( '', 'echo "<div class=\"error\"><p>Error: you have already activated <strong>Hacklog Remote Attachment</strong>,Please deactivate it and then activate this plugin(<strong>Hacklog Remote Attachment Upyun</strong>).</p></div>";' ) );
+}
+}
+add_action('admin_init','check_hacklog_ra_upyun_compability');
+
+
+define ( 'HACKLOG_RA_UPYUN_LOADER', __FILE__ );
+require plugin_dir_path ( __FILE__ ) . '/includes/hacklogra_upyun.class.php';
+new hacklogra_upyun ();
